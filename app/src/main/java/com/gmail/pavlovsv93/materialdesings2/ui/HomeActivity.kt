aa855428f8dfd1699312a806dd6206dc5c1a72b3
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-05-20 04:20:38 +0300

    более сложный <<простой холодный старт>>. избавление от фона windows

--- app/src/main/java/com/gmail/pavlovsv93/materialdesings2/ui/HomeActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/materialdesings2/ui/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.gmail.pavlovsv93.materialdesings2.ui
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gmail.pavlovsv93.materialdesings2.R
@@ -8,6 +9,7 @@ import com.gmail.pavlovsv93.materialdesings2.ui.recyclerviewfragment.RecyclerVie
 class HomeActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		setTheme(R.style.Theme_MaterialDesingS2)
 		setContentView(R.layout.activity_home)
 		if(savedInstanceState == null){
 			supportFragmentManager.beginTransaction()
